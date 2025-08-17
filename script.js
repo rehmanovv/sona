5bc66a93db8d9a015a37e57480acc2ad2c7eeccb
rehmanovv <tvsayfasi.rtr@gmail.com>
2025-08-17 22:34:45 +0400

    script.js

--- script.js
+++ script.js
@@ -587,10 +587,10 @@ scene.add(planet);
 
 //Ganti Deskripsi Planet
 const ringTexts = [
-  'Galaxy of love From ...',//untuk deskripsi planet layer 1
-  "I love you",//untuk deskripsi planet layer 2
-  "♡Happy Girlfriend day♡",//untuk deskripsi planet layer 3
-  "01/08/2025",//untuk deskripsi planet layer 4
+  "08.10.2025",
+  "Sona seni çox sevirem🤍",
+  "Yaxşıki varsan",
+  "Herşeyimsen🤍",
   ...(window.dataCCD && window.dataCCD.data.ringTexts ? window.dataCCD.data.ringTexts : [])
 ];
 
